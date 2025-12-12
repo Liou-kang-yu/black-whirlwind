34a12de72583d771025f89239f7e665bea5d202d
Liou-kang-yu <87876666gg@gmail.com>
2025-12-12 11:23:18 +0800

    Update 50+100總和.py

--- 50+100總和.py
+++ 50+100總和.py
@@ -1,6 +1,5 @@
-sum_1_to_100 = funtion(100)
-sum_1_to_49 = funtion(49)
-sum_50_to_100 = sum_1_to_100 - sum_1_to_49
-#(50+100總和)=(1+100總和)-(1+49總和)
-
+sum_upper = funtion(100)
+sum_lower_exclusive = funtion(49)
+sum_50_to_100 = sum_upper - sum_lower_exclusive
+#50+100總和=1+100總和-1+49總和
 print(f"從 50 加到 100 的總和為: {sum_50_to_100}")
