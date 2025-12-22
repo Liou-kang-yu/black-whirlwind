97f68f59b7900cc599681b132ba0e69994ba4fbc
u1311004 <u1311004@gmail.com>
2025-12-22 16:06:14 +0800

    產生雜湊

--- MYfunction.py
+++ MYfunction.py
@@ -1,4 +1,7 @@
-def funtion(n):
-    if n < 0:
-        return 0
-    return n * (n + 1) // 2
+import bcrypt
+def hash_password(password):
+
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()                   
+    hashed_pw = bcrypt.hashpw(password_bytes, salt) 
+    return hashed_pw.decode('utf-8')           
