6591bd111facf7e3e946830244a4648c2ffa10fd
Liou-kang-yu <87876666gg@gmail.com>
2025-12-22 17:02:14 +0800

    Create hash_password.py

---
+++ hash_password.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+def hash_password(password):
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()                   
+    hashed_pw = bcrypt.hashpw(password_bytes, salt) 
+    return hashed_pw.decode('utf-8') 
