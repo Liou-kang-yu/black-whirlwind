f700fbb07c5391081e385cf13247ab4b76b13e09
u1311004 <u1311004@gmail.com>
2025-12-22 16:52:44 +0800

    Delete verify_password.py

--- verify_password.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-def verify_password(input_password, stored_hashed_password):
-    input_bytes = input_password.encode('utf-8')      
-    stored_hash_bytes = stored_hashed_password.encode('utf-8')
-    return bcrypt.checkpw(input_bytes, stored_hash_bytes) 
